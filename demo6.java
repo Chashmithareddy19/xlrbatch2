33559aa5b7297b0bfe734f6eca9bffa76a250ead
Chashmithareddy19 <chashmithareddy19@gmail.com>
2025-01-03 10:09:09 +0530

    Add files via upload

---
+++ demo6.java
@@ -0,0 +1,12 @@
+public class demo6 {
+        public static void main(String[] args) {
+            String username = "Chashmithareddy";
+            String pwd = "123456";
+            while((username == "Chashmithareddy") && (pwd == "123456")) {
+                System.out.println("hello Chashmithareddy");
+                break;      
+              }
+        }
+    }
+    
+
